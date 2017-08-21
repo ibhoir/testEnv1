34f36b043796482c119eef85bdd19914801b3cd9
ibhoir <indrayani.bhoir@capgemini.com>
2017-08-21 16:59:18 +0530

    Update hello.js

--- src/main/webapp/hello.js
+++ src/main/webapp/hello.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('https://pwdutiltomcat-cposs-dev.eps-nonprd.corp.cox.com/getData/41').
+    $http.get('http://pwdutiltomcat:8080/getData/41').
         then(function(response) {
             $scope.greeting = response.data;
         });
